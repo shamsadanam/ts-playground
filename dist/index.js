0f22708aa84609d721b752cbe5ca1d630153366c
Shamsad Anam <shamsad.anam@gmail.com>
2022-10-05 22:10:43 +0600

    Learned Primitives and Arrays

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+// Basic/Primitive Types
+let id = 1;
+let username = "Six";
+let isOnline = true;
+// Arrays
+let number = [23, 34, 39, 30];
+let spcificOrder = [12, 1, "orange"];
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// Basic/Primitive Types
+let id: number = 1;
+let username: string = "Six";
+let isOnline: boolean = true;
+
+// Arrays
+let number: number[] = [23, 34, 39, 30];
+let spcificOrder: [number, boolean, string] = [12, 1, "orange"];
