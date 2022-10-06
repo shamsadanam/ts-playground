e8ca664ccec68e3ad2c83638cb1742ac63e701ee
shamsadanam <shamsad.anam@gmail.com>
2022-10-06 10:32:15 +0600

    learning

--- dist/index.js
+++ dist/index.js
@@ -5,4 +5,43 @@ let username = "Six";
 let isOnline = true;
 // Arrays
 let number = [23, 34, 39, 30];
-let spcificOrder = [12, 1, "orange"];
+let spcificOrder = [12, true, "orange"];
+//Tuple Array
+let arrOfArr = [
+    [1, "red"], [2, "blue"], [3, "green"]
+];
+//Enums
+var dir;
+(function (dir) {
+    dir[dir["UP"] = 0] = "UP";
+    dir[dir["DOWN"] = 1] = "DOWN";
+    dir[dir["LEFT"] = 2] = "LEFT";
+    dir[dir["RIGHT"] = 3] = "RIGHT";
+})(dir || (dir = {}));
+//Assertions
+let cid = 1;
+let customerId = cid;
+//Unions
+let pid = "1";
+pid = "one";
+let user = {
+    id: 1,
+    name: "red"
+};
+//functions 
+function add(a, b) {
+    return a + b;
+}
+add(3, 2);
+//Void Functions
+function log(msg, name) {
+    console.log(`Hello ${name}, ${msg}`);
+}
+let vipUser = {
+    id: 1,
+    name: "blue"
+};
+const addition = (a, b) => a + b;
+const division = (a, b) => a / b;
+addition(5, 7);
+division(90, 6);
--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,70 @@ let isOnline: boolean = true;
 
 // Arrays
 let number: number[] = [23, 34, 39, 30];
-let spcificOrder: [number, boolean, string] = [12, 1, "orange"];
+let spcificOrder: [number, boolean, string] = [12, true, "orange"];
+
+//Tuple Array
+let arrOfArr: [number, string][] = [
+    [1, "red"], [2, "blue"], [3, "green"]
+];
+
+//Enums
+enum dir {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+}
+
+//Assertions
+let cid: any = 1;
+let customerId = <number>cid;
+
+//Unions
+let pid: number | string = "1";
+pid = "one";
+
+// Objects
+type User = {
+    id: number,
+    name: string
+}
+
+let user: User = {
+    id: 1,
+    name: "red"
+}
+
+//functions 
+function add(a: number, b:number): number{
+    return a +b;
+}
+add(3, 2);
+
+//Void Functions
+function log(msg: string, name: string): void{
+    console.log(`Hello ${name}, ${msg}`);
+}
+
+//interfaces
+// interface can't use unions
+interface UserInterface {
+    id: number,
+    name: string,
+}
+
+let vipUser: UserInterface = {
+    id: 1,
+    name: "blue"
+}
+
+interface MathFunc {
+    (x: number, y: number) : number
+}
+
+const addition: MathFunc = (a, b) => a + b;
+const division: MathFunc = (a, b) => a / b;
+
+
+addition(5, 7);
+division(90, 6);
